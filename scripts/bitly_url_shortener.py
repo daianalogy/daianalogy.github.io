6df8a425e994bd5b794753a32799e8fca43b83f7
Jessica Daiana <daianalogy@gmail.com>
2023-01-12 11:24:20 -0400

    updating script content

--- scripts/bitly_url_shortener.py
+++ scripts/bitly_url_shortener.py
@@ -1,3 +1,14 @@
-colors = ["red", "white", "blue"]
-colors.insert(2, "yellow")
-print(colors)
+import requests
+
+
+headers = {
+    'Authorization': '54b1691adf0cf8e78fc49bdff7803ec36fd2555e',
+    'Content-Type': 'application/json',
+}
+
+data = '{ "long_url": "https://github.com/ashutosh1919/masterPortfolio.git" }'
+
+response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+url = response.json()
+
+print(url['link'])
