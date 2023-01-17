f5d47dce5d2d68bfc50b5be1ca323333bb8cb12b
Jessica Daiana <daianalogy@gmail.com>
2023-01-17 09:00:23 -0400

    (ENG) This is my a metadata editor script, currently working with .mp3 files.
    there are two main functions for now.
    
    1. collect(): It collects the directory path where the songs are stored by
    input, checks that it is not empty, and then proceeds to ask for
    the album's title to be modified.
    
    2. autotag(): It is executed with the previously requested parameters and
    formats the name of the .mp3 file to add it to the metadata section:
    title, album.
    
    (SPA) Este es un editor de metadatos actualmente
    funcionando en archivos .mp3
    
    Está compuesto por ahora de dos funciones:
    
    1. collect(): Recolecta por entrada la ruta del directorio dónde
    se almacenan las canciones, verifica que no esté vacio y luego
    procede a pedir el nombre del álbum a modificar.
    
    2. autotag(): Se ejecuta con los parámetros previamente solicitados
    y formatea el nombre del archivo .mp3 para añadirlo al la seccion
    de los metadatos: título, álbum.

---
+++ scripts/autotag.py
@@ -0,0 +1,49 @@
+import os
+import re
+import mutagen
+import sys
+
+#filename.endswith('.mp3') and
+
+def autotag(path, album):
+    for filename in os.listdir(path):
+        try:
+            if re.match(r'^\d+', filename):
+                file_path = os.path.join(path, filename)
+                audio = mutagen.File(file_path)
+                song_number = re.search(r'\d+', filename).group()
+                song_name =os.path.splitext(filename)[0].replace(song_number + '.','').strip()
+        
+                # Aquí reemplaza el titulo de la canción por el nombre del archivo
+                audio.tags['TIT2'] = mutagen.id3.TIT2(encoding=2, text=[song_name])
+                # Aquí se establece el nombre del álbum entero
+                audio.tags['TALB'] = mutagen.id3.TALB(encoding=2, text=[album])
+    
+                audio.save()
+        except Exception as e:
+            print("Cambia los permisos de tu carpeta e intenta nuevamente.")
+            break
+        else:
+            print(F"Se actualizó metadatos de la canción: {song_name} correctamente.")
+   
+def collect():
+    global path
+    global album
+    path = str(input("Proporcione la ruta de la carpeta: "))
+    #Comprueba que la ruta exista sino pide corregir y cierra
+    if os.path.exists(path):
+        print("El directorio es válido...")
+    # Comprueba que el directorio esté lleno.
+        if len(os.listdir(path)) <= 0:
+            print("...pero está vació, por favor, agrege las canciones en formato .mp3 e intente nuevamente")   
+            sys.exit()
+        else:
+            album = str(input("Ahora proporcione el nombre del álbum: "))        
+    else:
+        print("Por favor copie y pegue la ruta completa desde C: ")
+        sys.exit()
+
+collect()
+autotag(path, album)
+
+
